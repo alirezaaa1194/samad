7027b8a851fa2a45822fa7dd89971da0d0830b97
Alireza <alirezaaa1194@gmail.com>
2025-06-07 03:33:28 +0330

    fix card responsive

--- src/components/Reserver/Calender/Foods.tsx
+++ src/components/Reserver/Calender/Foods.tsx
@@ -13,11 +13,10 @@ function Foods({ groupedFoods, weekOffset }: { groupedFoods: groupFoodType; week
   const [mainFood, setMainFood] = useState<foodType | null>(null);
   const user = useUser();
   const { Canvas } = useQRCode();
-
   const isUserReserved = user?.reserved_foods.filter((userFood) => userFood.food === mainFood?.id);
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 items-center p-2">
+    <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-2 items-center p-2">
       <Modal isOpen={isOpen} size="lg" onClose={onClose} placement="center" className="!rounded-[10px] h-fit m-5">
         <ModalContent>
           {(onClose) => (
@@ -60,7 +59,7 @@ function Foods({ groupedFoods, weekOffset }: { groupedFoods: groupFoodType; week
         </ModalContent>
       </Modal>
       {days.map((day, j) => (
-        <div className="flex flex-col gap-2" key={day.key}>
+        <div className="flex md:flex-col gap-2" key={day.key}>
           {groupedFoods
             .filter((group) => day.key === dayjs(dayjs(group[0]).toDate()).format("YYYY-MM-DD"))[0]?.[1][0]
             .foods.map((food: foodType, i: number) => (
@@ -69,9 +68,7 @@ function Foods({ groupedFoods, weekOffset }: { groupedFoods: groupFoodType; week
                   <span className="text-[#4c8251] text-sm font-DanaDemiBold">ناهار</span>
                   <span className="text-sm text-[#706e6e] font-DanaMedium">{food.food_type === "pl" ? "15,000" : "10,000"} تومان</span>
                 </div>
-                <span className="text-center text-black/90 font-DanaDemiBold text-sm mt-4">
-                  {food.predefined_food?.name || food.custom_name}
-                </span>
+                <span className="text-center text-black/90 font-DanaDemiBold text-sm mt-4">{food.predefined_food?.name || food.custom_name}</span>
                 <Button
                   onPress={() => {
                     setMainFood(food);
